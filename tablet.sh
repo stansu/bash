6643fe69692cef8269f2a41dd830c65f2e80e6b9
stan <stan.stz@gmail.com>
2017-03-03 22:14:18 +0800

    add tablet.sh

---
+++ tablet.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+xsetwacom set "Wacom Intuos PT S 2 (WL) Pad pad" Button 1 "key +ctrl ] -ctrl"
+xsetwacom set "Wacom Intuos PT S 2 (WL) Pad pad" Button 3 "key +ctrl [ -ctrl"
+xsetwacom set "Wacom Intuos PT S 2 (WL) Pad pad" Button 8 "key +ctrl ] -ctrl"
+xsetwacom set "Wacom Intuos PT S 2 (WL) Pad pad" Button 9 "key +ctrl [ -ctrl"
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 1 "key +ctrl ] -ctrl"
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 3 "key +ctrl [ -ctrl"
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 8 "key +ctrl ] -ctrl"
+xsetwacom set "Wacom Intuos PT S 2 Pad pad" Button 9 "key +ctrl [ -ctrl"
